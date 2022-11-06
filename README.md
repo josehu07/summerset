7067bd40411a5c816b714448e77e36f76c1e729d
Guanzhou Hu <huguanzhou123@sina.com>
2022-11-05 21:27:17 -0500

    add sample unit tests and CI

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Summerset
 
+![build](https://github.com/josehu07/summerset/actions/workflows/build.yml/badge.svg)
+
 Summerset is a distributed key-value store built to incorporate a wide range of state machine replication (SMR) protocols for research purposes.
 
 TODO
--- src/statemach.rs
+++ src/statemach.rs
@@ -1,11 +1,12 @@
 use std::collections::HashMap;
 
-#[derive(Clone)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Command {
     Get { key: String },
     Put { key: String, value: String },
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum CommandResult {
     GetResult { value: Option<String> },
     PutResult { old_value: Option<String> },
@@ -40,5 +41,101 @@ impl StateMachine {
 
     fn exec_put(&mut self, key: String, value: String) -> Option<String> {
         self.data.insert(key, value)
+    }
+}
+
+#[cfg(test)]
+mod statemach_tests {
+    use super::{Command, CommandResult, StateMachine};
+    use std::collections::HashMap;
+    use rand::prelude::*;
+
+    #[test]
+    fn get_empty() {
+        let mut sm = StateMachine::new();
+        assert_eq!(
+            sm.execute(&Command::Get { key: "Jose".into() }),
+            CommandResult::GetResult { value: None }
+        );
+    }
+
+    #[test]
+    fn put_one_get_one() {
+        let mut sm = StateMachine::new();
+        assert_eq!(
+            sm.execute(&Command::Put {
+                key: "Jose".into(),
+                value: "180".into(),
+            }),
+            CommandResult::PutResult { old_value: None }
+        );
+        assert_eq!(
+            sm.execute(&Command::Get { key: "Jose".into() }),
+            CommandResult::GetResult {
+                value: Some("180".into())
+            }
+        );
+    }
+
+    #[test]
+    fn put_twice() {
+        let mut sm = StateMachine::new();
+        assert_eq!(
+            sm.execute(&Command::Put {
+                key: "Jose".into(),
+                value: "180".into()
+            }),
+            CommandResult::PutResult { old_value: None }
+        );
+        assert_eq!(
+            sm.execute(&Command::Put {
+                key: "Jose".into(),
+                value: "185".into()
+            }),
+            CommandResult::PutResult {
+                old_value: Some("180".into())
+            }
+        )
+    }
+
+    fn gen_rand_str(len: usize) -> String {
+        rand::thread_rng()
+            .sample_iter(&rand::distributions::Alphanumeric)
+            .take(len)
+            .map(char::from)
+            .collect()
+    }
+
+    #[test]
+    fn put_rand_get_rand() {
+        let mut sm = StateMachine::new();
+        let mut ref_sm = HashMap::<String, String>::new();
+        for _ in 0..100 {
+            let key = gen_rand_str(1);
+            let value = gen_rand_str(10);
+            assert_eq!(
+                sm.execute(&Command::Put {
+                    key: key.clone(),
+                    value: value.clone()
+                }),
+                CommandResult::PutResult {
+                    old_value: ref_sm.insert(key, value)
+                }
+            );
+        }
+        let keys: Vec<&String> = ref_sm.keys().collect();
+        for _ in 0..100 {
+            let key: String = if rand::random() {
+                (*keys.choose(&mut rand::thread_rng()).unwrap()).into()
+            } else {
+                "nonexist!".into()
+            };
+            assert_eq!(
+                sm.execute(&Command::Get { key: key.clone() }),
+                CommandResult::GetResult {
+                    value: ref_sm.get(&key).cloned()
+                }
+            )
+        }
     }
 }
