30c07e98960296d244b31872f0848e6257ab194a
Guanzhou Hu <huguanzhou123@sina.com>
2023-01-25 19:49:18 +0800

    remove cargo features due to non-intuitive semantics

--- src/protocols/mod.rs
+++ src/protocols/mod.rs
@@ -9,14 +9,10 @@ use crate::replicator::{
 };
 use crate::utils::InitError;
 
-#[cfg(feature = "protocol_do_nothing")]
 mod do_nothing;
-#[cfg(feature = "protocol_do_nothing")]
 use do_nothing::{DoNothingServerNode, DoNothingCommService, DoNothingClientStub};
 
-#[cfg(feature = "protocol_simple_push")]
 mod simple_push;
-#[cfg(feature = "protocol_simple_push")]
 use simple_push::{
     SimplePushServerNode, SimplePushCommService, SimplePushClientStub,
 };
@@ -48,68 +44,47 @@ impl SMRProtocol {
     }
 
     /// Create a server replicator module instance of this protocol on heap.
-    #[allow(unused_variables, unreachable_patterns)]
     pub fn new_server_node(
         &self,
         peers: Vec<String>,
     ) -> Result<Box<dyn ReplicatorServerNode>, InitError> {
         match self {
-            #[cfg(feature = "protocol_do_nothing")]
             Self::DoNothing => {
                 box_if_ok!(DoNothingServerNode::new(peers))
             }
-            #[cfg(feature = "protocol_simple_push")]
             Self::SimplePush => {
                 box_if_ok!(SimplePushServerNode::new(peers))
             }
-            _ => Err(InitError(format!(
-                "protocol {} is not enabled in cargo features",
-                self
-            ))),
         }
     }
 
     /// Create a server internal communication tonic service holder struct.
-    #[allow(unused_variables, unreachable_patterns)]
     pub fn new_comm_service(
         &self,
         node: Arc<SummersetServerNode>,
     ) -> Result<Box<dyn ReplicatorCommService>, InitError> {
         match self {
-            #[cfg(feature = "protocol_do_nothing")]
             Self::DoNothing => {
                 box_if_ok!(DoNothingCommService::new(node))
             }
-            #[cfg(feature = "protocol_simple_push")]
             Self::SimplePush => {
                 box_if_ok!(SimplePushCommService::new(node))
             }
-            _ => Err(InitError(format!(
-                "protocol {} is not enabled in cargo features",
-                self
-            ))),
         }
     }
 
     /// Create a client replicator stub instance of this protocol on heap.
-    #[allow(unused_variables, unreachable_patterns)]
     pub fn new_client_stub(
         &self,
         servers: Vec<String>,
     ) -> Result<Box<dyn ReplicatorClientStub>, InitError> {
         match self {
-            #[cfg(feature = "protocol_do_nothing")]
             Self::DoNothing => {
                 box_if_ok!(DoNothingClientStub::new(servers))
             }
-            #[cfg(feature = "protocol_simple_push")]
             Self::SimplePush => {
                 box_if_ok!(SimplePushClientStub::new(servers))
             }
-            _ => Err(InitError(format!(
-                "protocol {} is not enabled in cargo features",
-                self
-            ))),
         }
     }
 }
