4514ac611dcecb7b3bf76aedd21bf501783915a1
Guanzhou Hu <huguanzhou123@sina.com>
2023-09-28 22:41:43 -0500

    update constraint boundary figure script

--- perf_sim/plot_cstr_bounds.py
+++ perf_sim/plot_cstr_bounds.py
@@ -2,6 +2,7 @@ import matplotlib  # type: ignore
 
 matplotlib.use("Agg")
 
+import numpy as np  # type: ignore
 import matplotlib.pyplot as plt  # type: ignore
 import matplotlib.patches as mpatches  # type: ignore
 from matplotlib.legend_handler import HandlerPatch  # type: ignore
@@ -12,9 +13,9 @@ SUBPLOT_ARG = lambda idx: 141 + idx
 
 CLUSTER_SIZES = [3, 5, 7, 9]
 SIZE_COLOR_MAP = {
-    3: ("orange", "bisque"),
-    5: ("seagreen", "lightgreen"),
-    7: ("steelblue", "skyblue"),
+    3: ("seagreen", "palegreen"),
+    5: ("orange", "bisque"),
+    7: ("steelblue", "powderblue"),
     9: ("chocolate", "mistyrose"),
 }
 
@@ -33,7 +34,7 @@ def plot_cstr_bound(idx, cluster_size):
 
     # Classic Paxos/Raft point
     plt.scatter(
-        m, m, marker="D", s=100, color="dimgray", label="Classic Paxos/Raft", zorder=10
+        m, m, marker="s", s=100, color="black", label="Classic Paxos/Raft", zorder=10
     )
 
     # CRaft point
@@ -42,7 +43,7 @@ def plot_cstr_bound(idx, cluster_size):
         craft_q,
         1,
         marker="X",
-        s=100,
+        s=110,
         color="lightcoral",
         label="RS-Paxos/CRaft",
         zorder=10,
@@ -61,13 +62,13 @@ def plot_cstr_bound(idx, cluster_size):
         label="Crossword configs",
         zorder=20,
     )
-    plt.vlines(m, ymin=m, ymax=m + 1.5, linestyles="--", color=line_color, zorder=20)
-    plt.vlines(n, ymin=1, ymax=2.5, linestyles="--", color=line_color, zorder=20)
+    plt.vlines(m, ymin=m, ymax=m + 1.5, linestyles="-", color=line_color, zorder=20)
+    plt.vlines(n, ymin=1, ymax=2.5, linestyles="-", color=line_color, zorder=20)
 
     # correct region
     xs = [m, m, n, n]
     ys = [m, m + 1, 2, 1]
-    plt.fill(xs, ys, color=fill_color, label="Correct region", zorder=0)
+    plt.fill(xs, ys, color=fill_color, label="Region of fault-tolerance=f", zorder=0)
 
     # latency & throughput optimized arrows
     plt.arrow(
@@ -83,8 +84,8 @@ def plot_cstr_bound(idx, cluster_size):
         label="Tradeoff decisions",
     )
     plt.text(
-        m + 0.18 if n == 3 else m + 0.5 if n == 9 else m + 0.4,
-        m + 2.78 if n == 3 else m + 2.0 if n == 9 else m + 2.4,
+        m + 0.18 if n <= 5 else m + 0.5 if n == 9 else m + 0.4,
+        m + 2.78 if n <= 5 else m + 2.0 if n == 9 else m + 2.4,
         "Lat.\noptim.",
         horizontalalignment="left",
         verticalalignment="center",
@@ -102,8 +103,8 @@ def plot_cstr_bound(idx, cluster_size):
         overhang=0.5,
     )
     plt.text(
-        n + 0.8 if n == 3 else n + 0.0 if n == 9 else n + 0.4,
-        1 + 1.5 if n == 3 else 1 + 2.9 if n == 9 else 1 + 2.6,
+        n + 0.8 if n <= 5 else n + 0.0 if n == 9 else n + 0.4,
+        1 + 1.5 if n <= 5 else 1 + 2.9 if n == 9 else 1 + 2.6,
         "Tput.\noptim.",
         horizontalalignment="left",
         verticalalignment="center",
@@ -126,7 +127,7 @@ def plot_cstr_bound(idx, cluster_size):
 
     plt.title(
         f"|Cluster|={n}  f={f}",
-        x=0.27,
+        x=0.3,
         y=-0.38,
         fontsize=10,
         fontweight="bold",
@@ -134,6 +135,64 @@ def plot_cstr_bound(idx, cluster_size):
     )
 
     return ax
+
+
+def make_legend(fig, handles, labels):
+    def make_legend_arrow(
+        legend, orig_handle, xdescent, ydescent, width, height, fontsize
+    ):
+        return mpatches.FancyArrow(
+            0,
+            0.5 * height,
+            width,
+            0,
+            linewidth=1,
+            color="dimgray",
+            length_includes_head=True,
+            head_width=0.6 * height,
+            overhang=0.3,
+        )
+
+    def make_legend_polygon(
+        legend, orig_handle, xdescent, ydescent, width, height, fontsize
+    ):
+        return mpatches.Polygon(
+            xy=np.array(
+                [
+                    [0.2 * width, 0.5 * height],
+                    [0.2 * width, 1.2 * height],
+                    [0.8 * width, 0.5 * height],
+                    [0.8 * width, -0.2 * height],
+                ]
+            ),
+            closed=True,
+            color="dimgray",
+        )
+
+    order = []
+    for s in ("Classic", "RS-", "Crossword", "Region", "Tradeoff"):
+        for i, l in enumerate(labels):
+            if s in l:
+                order.append(i)
+                break
+    sorted_handles = [handles[i] for i in order]
+    sorted_labels = [labels[i] for i in order]
+
+    leg = fig.legend(
+        sorted_handles,
+        sorted_labels,
+        loc="lower center",
+        bbox_to_anchor=(0.5, 0.78),
+        ncol=len(handles),
+        handlelength=1.5,
+        handletextpad=0.5,
+        handler_map={
+            mpatches.FancyArrow: HandlerPatch(patch_func=make_legend_arrow),
+            mpatches.Polygon: HandlerPatch(patch_func=make_legend_polygon),
+        },
+    )
+    for h in leg.legend_handles[2:]:
+        h.set_color("dimgray")
 
 
 def plot_all_cstr_bounds():
@@ -152,34 +211,8 @@ def plot_all_cstr_bounds():
         if idx == len(CLUSTER_SIZES) - 1:
             handles, labels = ax.get_legend_handles_labels()
 
-    def make_legend_arrow(
-        legend, orig_handle, xdescent, ydescent, width, height, fontsize
-    ):
-        return mpatches.FancyArrow(
-            0,
-            0.5 * height,
-            width,
-            0,
-            linewidth=1,
-            color="dimgray",
-            length_includes_head=True,
-            head_width=0.75 * height,
-            overhang=0.3,
-        )
-
     # single legend group on top
-    handles = handles[-2:] + handles[:-2]
-    labels = labels[-2:] + labels[:-2]
-    fig.legend(
-        handles,
-        labels,
-        loc="lower center",
-        bbox_to_anchor=(0.5, 0.78),
-        ncol=len(handles),
-        handlelength=1.5,
-        handletextpad=0.5,
-        handler_map={mpatches.FancyArrow: HandlerPatch(patch_func=make_legend_arrow)},
-    )
+    make_legend(fig, handles, labels)
 
     plt.tight_layout()
     plt.savefig(f"results/cstr_bounds.png", dpi=300)
