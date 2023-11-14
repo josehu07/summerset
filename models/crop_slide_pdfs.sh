c8f5c9deccc06aa2151e4077a9d2997c91a5e250
Guanzhou Hu <huguanzhou123@sina.com>
2023-11-14 01:43:21 -0600

    update pdf cropping script

--- models/crop_slide_pdfs.sh
+++ models/crop_slide_pdfs.sh
@@ -6,18 +6,15 @@
 
 # Usage:
 #    1. save slides exported PDF as results/slide-figures.pdf
-#    2. run: ./models/crop_slides_pdfs.sh results/slide-figures.pdf <take_pages>
+#    2. run: ./models/crop_slides_pdfs.sh
+
+ORIGINAL_PDF=results/slide-figures.pdf
+TAKE_PAGES=8
 
 
-if [ $# -ne 2 ];
-then
-    echo "Usage: ./models/crop_slides_pdfs.sh results/slide-figures.pdf <take_pages>"
-    exit 1
-fi
-
-ORIGINAL_PDF=$1
-TAKE_PAGES=$2
-
+echo
+echo "Deleting old results..."
+rm results/slides/*.pdf
 
 echo
 echo "Separating desired pages..."
@@ -25,17 +22,30 @@ pdfseparate -l $TAKE_PAGES $ORIGINAL_PDF "results/slides/slide-%d.pdf"
 
 
 echo
-echo "Cropping pages individual PDFs..."
+echo "Cropping pages into separate PDFs..."
 for FILE in $(ls results/slides/ | grep .pdf);
 do
     echo "    cropping $FILE"
-    pdfcropmargins -p 5 -mo -o results/slides "results/slides/$FILE"
+    pdfcropmargins -p 0 -mo -o results/slides "results/slides/$FILE"
 done
 
 echo
-echo "Deleting old uncropped files..."
+echo "Deleting uncropped files..."
 for FILE in $(ls results/slides/ | grep _uncropped.pdf);
 do
     rm "results/slides/$FILE"
+done
+rm "results/slide-figures.pdf:Zone.Identifier"
+
+echo
+echo "Renaming cropped files..."
+TARGET_NAMES=("status_diagram" "log_in_action" "rs_codeword_space" "policy-multipaxos"
+              "policy-rspaxos" "policy-balanced_rr" "policy-unbalanced" "concurrent_failures")
+for IDX in ${!TARGET_NAMES[@]};
+do
+    OLD_NAME="slide-$((IDX+1)).pdf"
+    NEW_NAME="${TARGET_NAMES[$IDX]}.pdf"
+    echo "    renaming $OLD_NAME to $NEW_NAME"
+    mv "results/slides/$OLD_NAME" "results/slides/$NEW_NAME"
 done
 echo
