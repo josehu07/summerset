f009e6d2a2483dd8513227f5e8934ce03e14badc
Guanzhou Hu <me@josehu.com>
2024-03-29 00:05:33 +0800

    updating CloudLab experiments support

--- scripts/setup/geni_dev_local.py
+++ scripts/setup/geni_dev_local.py
@@ -1,20 +1,17 @@
-"""Running Summerset on a variable number of c220g5 nodes in a single site.
+"""Running Summerset on a variable number of nodes in a single site.
 """
 
 # Import the Portal object.
-import geni.portal as portal
+import geni.portal as portal  # type: ignore
+
+# Primary partition's disk image.
+DISK_IMAGE = "urn:publicid:IDN+wisc.cloudlab.us+image+advosuwmadison-PG0:summerset.dev"
 
 # Create a portal context, needed to defined parameters.
 pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
-
-# Fixed parameters.
-PHYS_NODE_TYPE = "c220g5"
-DISK_IMAGE = (
-    "urn:publicid:IDN+wisc.cloudlab.us+image+advosuwmadison-PG0:summerset.c220g5"
-)
 
 # Variable number of nodes.
 pc.defineParameter(
@@ -24,12 +21,26 @@ pc.defineParameter(
     5,
 )
 
+# Physical type for all nodes.
+pc.defineParameter(
+    "nodeType",
+    "Physical node type",
+    portal.ParameterType.STRING,
+    "c220g5",
+)
+
 # Retrieve the values the user specifies during instantiation.
 params = pc.bindParameters()
 
 # Check parameter validity.
 if params.nodeCount < 1 or params.nodeCount > 9:
     pc.reportError(portal.ParameterError("#nodes must be >= 1 and <= 9", ["nodeCount"]))
+
+if params.nodeType.strip() == "":
+    pc.reportError(portal.ParameterError("Physical node type is empty", ["nodeType"]))
+tokens = params.nodeType.split(",")
+if len(tokens) != 1:
+    pc.reportError(portal.ParameterError("Only a single type is allowed", ["nodeType"]))
 
 pc.verifyParameters()
 
@@ -45,7 +56,7 @@ for i in range(params.nodeCount):
     # Create a node and add it to the request
     name = "node" + str(i)
     node = request.RawPC(name)
-    node.hardware_type = PHYS_NODE_TYPE
+    node.hardware_type = params.nodeType
     node.disk_image = DISK_IMAGE
     # Add to lan
     if params.nodeCount > 1:
