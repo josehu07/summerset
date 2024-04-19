8eca21f538d334e0f6b85d8056991e51aa611325
Guanzhou Hu <huguanzhou123@sina.com>
2024-04-18 19:21:46 -0500

    update results archiving script

--- publish/crossword/archive_results.sh
+++ publish/crossword/archive_results.sh
@@ -20,18 +20,30 @@ fi
 
 TAR_NAME="$1.tar.xz"
 
+DROPBOX_DIR="~/Dropbox/UW-Madison/ADSL-Systems-Lab/Data-Backups/Crossword"
+
 
 if [ $# -ge 2 ] && [ "$2" = "ex" ];
 then
     # extracting...
+    echo
+    echo "Downloading archive from Dropbox..."
+    cp ${DROPBOX_DIR}/${TAR_NAME} backups/
+
+    echo
     echo "Extracting results/ <- backups/${TAR_NAME}..."
     tar -xf backups/${TAR_NAME} -C results/
 
 else
     # archiving...
+    echo
     echo "Archiving results/ -> backups/${TAR_NAME}..."
     cd results/
     tar -Jcf ${TAR_NAME} *
     cd ..
     mv results/${TAR_NAME} backups/
+
+    echo
+    echo "Replicating archive to Dropbox..."
+    cp backups/${TAR_NAME} ${DROPBOX_DIR}/
 fi
